dc72b4cc0914ff6ebd9829ea4ddb61a0024b3348
David Silver <dsilver@candidatemetrics.com>
2013-02-16 16:37:22 -0800

    add product_type to products

--- store/app/models/product.rb
+++ store/app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
-  attr_accessible :name, :price
+  attr_accessible :name, :price, :properties, :product_type_id
+  belongs_to :product_type
+  serialize :properties, Hash
 end
